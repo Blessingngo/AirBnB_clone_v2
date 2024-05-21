b2ec166e5ec05a6811d33aac67ecfb6bffd90832
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-05-21 04:09:40 +0100

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-from flask import Flask, render_template
+"""Start web application with two routings
+"""
+
 from models import storage
 from models.state import State
-from models.city import City
-
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/cities_by_states')
+def states_list():
+    """Render template with states
+    """
+    path = '8-cities_by_states.html'
+    states = storage.all(State)
+
+    # sort State object alphabetically by name
+    # sorted_states = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, states=states)
+
+
 @app.teardown_appcontext
-def tear_down(self):
-    """tear down app context"""
+def app_teardown(arg=None):
+    """Clean-up session
+    """
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def list_states():
-    """lists states from database
-    Returns:
-        HTML
-    """
-    dict_states = storage.all(State)
-    all_states = []
-    for k, v in dict_states.items():
-        all_states.append(v)
-    return render_template('8-cities_by_states.html', all_states=all_states)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
