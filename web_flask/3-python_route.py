8dc5757ef78c8b248d8da989a75e0e4ee72b10fd
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2024-05-21 03:37:46 +0100

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,52 +1,40 @@
 #!/usr/bin/python3
+"""Start web application with two routings
+"""
+
 from flask import Flask
-"""class Flask"""
-
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """displays text
-    Returns:
-        text
+@app.route('/')
+def hello():
+    """Return string when route queried
     """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """displays text
-    Returns:
-        text
+@app.route('/hbnb')
+def hbnb():
+    """Return string when route queried
     """
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_C(text):
-    """displays text
-    Args:
-        text (str): text
-    Returns:
-        text
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """Return reformatted text
     """
-    return 'C %s' % text.replace('_', ' ')
+    return 'C ' + text.replace('_', ' ')
 
 
-@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    """displays text
-    Args:
-        text (str): text
-    Returns:
-        text
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """Reformat text based on optional variable
     """
-    return 'Python %s' % text.replace('_', ' ')
+    return 'Python ' + text.replace('_', ' ')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
